cb30390c84fc8b29f33be92f8f08c60a80003936
brian <riceb53@gmail.com>
2020-06-25 20:29:34 -0400

    add weather api app

---
+++ weather.rb
@@ -0,0 +1,27 @@
+require 'http'
+
+p "enter a city name"
+user_input = gets.chomp
+
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{user_input}&units=metric&APPID=3c5ca86e43da691659345383d21f7d9b")
+
+weather_data = response.parse
+
+
+# print out the temperature in chicago
+temperature = weather_data['main']['temp']
+# print out the weather
+description = weather_data['weather'][0]['description']
+# print out the name "chicago"
+city_name = weather_data['name']
+# then turn that into a sentence
+
+p "it is #{temperature} degrees and #{description} in #{city_name} right now"
+
+# "it is 80 degrees and windy in chicago"
+
+
+
+# user enter in the name of a city
+# and we will tell them what the weather is there
+
